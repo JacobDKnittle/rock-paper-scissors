bd3a6e5ae15559382363ee239e92f1577d067714
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-21 16:31:14 -0700

    Add pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('hello');
+// get the computers choice
+    // use math.random to get a random number
+    // use random number in range to pick a choice
+    // returns rock, paper, or scissors
+
+// takes user choice and returns it
+    // prompt user for input and assume its valid, make sure its case insensitive
+    // test return with console.log
+    // return the choice
+
+// humanScore starting at 0
+
+// computerScore starting at 0
+
+// take human and computers choices into a function
+    // play a single round comparing choices and increase the winners score by 1
+    // log the winner of the round
+
+// playGame function that calls playRound 5 times
+    // move playRound and score Variables inside playGame
