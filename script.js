16f152b87ce3a0c7184f2629f33cb29d76d1e6e8
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-22 09:53:14 -0700

    Add js debugger file

--- script.js
+++ script.js
@@ -1,28 +1,52 @@
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber === 0) {
-        return 'Rock';
-    } else if (randomNumber === 1) {
-        return 'Paper';
-    } else if (randomNumber === 2) {
-        return 'Scissors';
-    }
+  let randomNumber = Math.floor(Math.random() * 3);
+  if (randomNumber === 0) {
+    return "rock";
+  } else if (randomNumber === 1) {
+    return "paper";
+  } else if (randomNumber === 2) {
+    return "scissors";
+  }
 }
 let computerChoice = getComputerChoice();
 
 function getHumanChoice() {
-    let userChoice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
-    return userChoice;   
+  let userChoice = prompt("Enter Rock, Paper, or Scissors").toLowerCase();
+  return userChoice;
 }
+
 let humanChoice = getHumanChoice();
 
 let humanScore = 0;
 let computerScore = 0;
 
-// take human and computers choices into a function
-    // play a single round comparing choices and increase the winners score by 1
-    // log the winner of the round
+function playRound(humanChoice, computerChoice) {
+  // play a single round comparing choices and increase the winners score by 1
+  // scenarios human wins
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === 'paper')
+  ) {
+    humanScore++;
+    console.log("Player wins round.");
+  }
+  // scenarios computer wins
+  else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === 'paper')
+  ) {
+    computerScore++;
+    console.log("Computer wins round.");
+  } else {
+    console.log("Tie!");
+  }
+  
+}
+
+playRound(humanChoice, computerChoice)
 
 // playGame function that calls playRound 5 times
-    // move playRound and score Variables inside playGame
+// move playRound and score Variables inside playGame
