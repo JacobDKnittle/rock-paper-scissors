788b6b02f0e9e78b99a5441d153d66ca54d4098b
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-26 11:57:33 -0700

    Update scores and result ui when choice selected

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const result = document.querySelector('.result');
+const playerScoreDisplay = document.querySelector('.playerScore');
+const computerScoreDisplay = document.querySelector('.computerScore');
+
+playerScoreDisplay.textContent = `Player Score: ${humanScore}`;
+computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+
 const choiceButtons = document.querySelectorAll('button');
 choiceButtons.forEach((button) => {
   button.addEventListener('click', () => {
-    console.log(button.textContent.toLowerCase());
-    playRound(button.textContent)
+    result.textContent = 'Result: ';
+    playRound(button.textContent.toLowerCase())
   })
 })
 
@@ -24,6 +31,7 @@ function getComputerChoice() {
 // play a single round comparing choices and increase the winners score by 1
 function playRound(humanSelection) {
   let humanChoice = humanSelection;
+  // console.log(humanChoice)
   let computerChoice = getComputerChoice();
   // scenarios human wins
   if (
@@ -31,8 +39,10 @@ function playRound(humanSelection) {
     (humanChoice === "paper" && computerChoice === "rock") ||
     (humanChoice === "scissors" && computerChoice === 'paper')
   ) {
-    console.log("Player wins round.");
-    return 'player wins round'
+    // console.log("Player wins round.");
+    humanScore++;
+    playerScoreDisplay.textContent = `Player Score: ${humanScore}`;
+    result.textContent += 'Player wins round!';
   }
   // scenarios computer wins
   else if (
@@ -40,25 +50,27 @@ function playRound(humanSelection) {
     (computerChoice === "paper" && humanChoice === "rock") ||
     (computerChoice === "scissors" && humanChoice === 'paper')
   ) {
-    console.log("Computer wins round.");
-    return 'computer wins round'
+    // console.log("Computer wins round.");
+    computerScore++;
+    computerScoreDisplay.textContent = `Computer Score: ${computerScore}`;
+    result.textContent += 'Computer wins round!';
   } else {
-    console.log("Tie!");
+    result.textContent += 'Tie!';
   }
   
 }
 
-function determineWinner(humanScore, computerScore){
-  if (humanScore > computerScore){
-    console.log('Player Wins Game!')
-  }
-  else if (computerScore > humanScore){
-    console.log('Computer Wins Game!');
-  }
-  else {
-    console.log('Game is a Tie!');
-  }
-}
+// function determineWinner(humanScore, computerScore){
+//   if (humanScore > computerScore){
+//     console.log('Player Wins Game!')
+//   }
+//   else if (computerScore > humanScore){
+//     console.log('Computer Wins Game!');
+//   }
+//   else {
+//     console.log('Game is a Tie!');
+//   }
+// }
 
 
 
